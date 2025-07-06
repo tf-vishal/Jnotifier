f5008ad7f86a269e048d5e6c48edebdd15cf7cfc
tf-vishal <vvishalsharma0609@gmail.com>
2025-07-06 13:47:08 +0530

    done

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadEnv() {
-	err := godotenv.Load(".env")
+
 	if err := godotenv.Load(); err != nil {
 		log.Println("ℹ️ Skipping .env load: running in Railway or no local .env")
 	}
