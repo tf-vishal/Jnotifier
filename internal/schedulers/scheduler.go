807cfb2225a659ad10c3439f40bcfa48a5ed7176
tf-vishal <vvishalsharma0609@gmail.com>
2025-07-06 11:45:40 +0530

    schedulers added

---
+++ internal/schedulers/scheduler.go
@@ -0,0 +1,34 @@
+package schedulers
+
+import (
+	"log"
+	"time"
+
+	"github.com/tf-vishal/JNotifier/internal/api"
+	"github.com/tf-vishal/JNotifier/internal/discord"
+)
+
+func StartJobScheduler() {
+	ticker := timer.NewTicker(30 * time.Minute)
+
+	go func() {
+		for range ticker.C {
+			checkJobs()
+		}
+	}()
+
+	checkJobs()
+}
+
+func checkJobs() {
+	keywords := []string{"Network Engineer", "System Administrator", "DevOps", "Cloud", "CCNA"}
+	for _, keyword := range keywords {
+		go func(kw string) {
+			jobs := api.FetchFromJsearch(kw)
+			for _, job := range jobs {
+				discord.SendJobNotification(job)
+			}
+		}(keyword)
+	}
+	log.Println("Job check completed for keywords:", keywords)
+}
